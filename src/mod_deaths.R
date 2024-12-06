0740f9c6f24f946f65b7cbedd3ece453d13725b2
John Bryant <john.r.bryant@gmail.com>
2024-12-07 09:15:47 +1300

    modified deaths models

--- src/mod_deaths.R
+++ src/mod_deaths.R
@@ -1,28 +1,27 @@
 
-library(bage)
-library(dplyr, warn.conflicts = FALSE)
-library(poputils)
-library(command)
+suppressPackageStartupMessages({
+  library(bage)
+  library(dplyr)
+  library(poputils)
+  library(command)
+})
 
 cmd_assign(deaths = "out/deaths.rds",
            popn = "out/popn.rds",
            .out = "out/mod_deaths.rds")
 
 data <- inner_join(deaths, popn, by = c("age", "sex", "time")) |>
-  mutate(age = combine_age(age, to = "five"),
-         age = set_age_open(age, lower = 60)) |>
+  mutate(age = combine_age(age, to = "five")) |>
   group_by(age, sex, time) |>
   summarise(deaths = sum(deaths), popn = sum(popn),
             .groups = "drop")
 
-
-mod <- mod_pois(deaths ~ age * sex + age * time + sex * time,
+mod <- mod_pois(deaths ~ age:sex + age:time + sex:time + time,
                 data = data,
                 exposure = popn) |>
-  set_prior(age ~ RW2()) |>
-  set_prior(age:sex ~ RW()) |>
-  set_prior(age:time ~ SVD_RW(HMD, zero_sum = TRUE)) |>
-  set_prior(sex:time ~ RW(zero_sum = TRUE)) |>
+  set_prior(age:sex ~ RW2_Infant()) |>
+  set_prior(age:time ~ SVD_RW2(HMD, zero_sum = TRUE)) |>
+  set_prior(sex:time ~ RW2(zero_sum = TRUE)) |>
   set_prior(time ~ Lin_AR()) |>
   fit()
 
